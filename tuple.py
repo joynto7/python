1194bc42a502f88ba510d18faaf6b185ad6bd547
joynto7 <joyntoghosh7@gmail.com>
2025-10-24 14:58:39 +0600

    Learning Tuple

---
+++ tuple.py
@@ -0,0 +1,10 @@
+#tuple is immutable ,so we can not change its values
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(type(thistuple))
+print(len(thistuple))
+print(range (thistuple))
+
+#len() function to get the length of the tuple 
+#type() function so knwo the data type of the varaible
+#range() function to get the range of the tuple
