bc0e70771319a400f32e261bc8fd425982e772c5
joynto7 <joyntoghosh7@gmail.com>
2025-10-26 12:21:05 +0600

    using union() method

--- set.py
+++ set.py
@@ -17,3 +17,9 @@ for x in theset:
 print("banana" in theset)
 print("orange" in theset)
 print("apple"in theset)
+
+# using union() method to join two sets
+set1 ={2,4,3,4,3,9,8}
+set2 ={5,3,8,6,1,6,}
+set3 = set1.union(set2)
+
