042bc5db46e27b1d0aadc551693a05e3b3a11bca
joynto7 <joyntoghosh7@gmail.com>
2025-10-30 13:05:27 +0600

    using loop in tuple

--- tuple_unpack.py
+++ tuple_unpack.py
@@ -3,3 +3,7 @@
 fruits = ('apple','banana','cherry', 'orange')
 (red,yellow,green,orange) = fruits 
 print(red,yellow,green,orange)
+
+#using loop tuple
+for i in range(len(fruits)):
+    print(fruits[i])
