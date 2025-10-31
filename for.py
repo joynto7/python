6cb4ad8b6232695019a46b37260c47c5b7f790f8
joynto7 <joyntoghosh7@gmail.com>
2025-10-31 19:26:14 +0600

    Using for in python

---
+++ for.py
@@ -0,0 +1,11 @@
+colours = [ "red", "Yellow","black","white"]
+for x in colours :
+    if x == "black":
+     break
+    print(x)
+
+cities = ["Manikganj","Dhaka","Rajshahi","chittaganj"]
+for a in cities:
+   if a == "Dhaka":
+      break
+   print(a)
