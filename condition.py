769d8f6f57bc8521ed4bb6541d750be74df658c2
joynto7 <joyntoghosh7@gmail.com>
2025-10-31 18:52:44 +0600

    multiple statements

--- condition.py
+++ condition.py
@@ -5,3 +5,10 @@ if a > b:
     print("a is greate than b")
 else:
     print("b is greater than a")
+
+# multiple statements in if block
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
