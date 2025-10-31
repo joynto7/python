bb752cc920926752215777e10f5fa2c842582b84
joynto7 <joyntoghosh7@gmail.com>
2025-10-31 19:03:49 +0600

    Nested if

--- condition.py
+++ condition.py
@@ -19,3 +19,16 @@ if marks >= 80:
    print("Grade A+")
 elif marks < 80 and marks >=70:
    print("Grade A")
+
+#nested if statement 
+score = 85
+if score >= 50:
+   print("You passed the exam")
+elif score >= 75:
+   print("You passed with good marks")
+elif score >= 90:
+   print("You passed with excellent marks")
+elif score < 50 :
+   print("Ypu failed the exam")
+else:
+   print("invalid Score")
