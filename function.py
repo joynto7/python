350dfc3dc2f0cf0ff3f0254a069299115768c007
joynto7 <joyntoghosh7@gmail.com>
2025-10-31 19:38:22 +0600

    function of py

---
+++ function.py
@@ -0,0 +1,12 @@
+def my_function():
+  print("Hello from a function")
+  print("She is a housewife")
+
+my_function()
+
+# function for adding something 
+
+def add_numbers(a, b):
+    """Adds two numbers and returns the result."""
+    result = a + b
+    return result
